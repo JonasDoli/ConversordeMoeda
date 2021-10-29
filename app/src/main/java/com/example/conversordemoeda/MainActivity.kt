a822dd67304ce1fc2f5c44237f1524eeb8cacf1e
JonasDoli <jonasdf@contato.net>
2021-10-29 00:32:54 -0300

    Consumindo api

--- app/src/main/java/com/example/conversordemoeda/MainActivity.kt
+++ app/src/main/java/com/example/conversordemoeda/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.conversordemoeda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun converter(view: android.view.View) {
+        calcular()
+    }
+
+    fun calcular() {
+
+        var selectedCurrence = rgGupo
+        var checar = selectedCurrence.checkedRadioButtonId
+
+
+        var resultado = when (checar) {
+            R.id.rbUsd ->
+                "USD-BRL"
+
+
+            R.id.rbBtc ->
+                "BTC-BRL"
+
+
+            else -> "EUR-BRL"
+        }
+
+        var imput = etValor.text.toString()
+        if (imput.isEmpty())
+            return
+
+        Thread {
+            var url = URL(" https://economia.awesomeapi.com.br/json/ $resultado")
+            var conn = url.openConnection() as HttpsURLConnection
+            try {
+                val data = conn.inputStream.bufferedReader().readText()
+
+
+                runOnUiThread {
+
+
+                    tvValoConvertido.text = data
+
+                }
+
+            } finally {
+                conn.disconnect()
+            }
+
+        }.start()
+
+
     }
 }
